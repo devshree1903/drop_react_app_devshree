f33d91d9c8fa7168783bb3ed8b640ed3adbcaf28
devshree1903 <bharangdevshree@gmail.com>
2023-07-31 15:37:23 +0530

    Updates

---
+++ precache-manifest.71173771e723c20853223844a07232fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "115a76760d3becf47d9fcdd84220faa9",
+    "url": "/drop_react_app_devshree/index.html"
+  },
+  {
+    "revision": "32efdbb6d35c646a0d1c",
+    "url": "/drop_react_app_devshree/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "984ac69e36dac944ecb1",
+    "url": "/drop_react_app_devshree/static/css/main.e92339a5.chunk.css"
+  },
+  {
+    "revision": "32efdbb6d35c646a0d1c",
+    "url": "/drop_react_app_devshree/static/js/2.a3a6fd2f.chunk.js"
+  },
+  {
+    "revision": "984ac69e36dac944ecb1",
+    "url": "/drop_react_app_devshree/static/js/main.8b95b5f4.chunk.js"
+  },
+  {
+    "revision": "8568911855962333b57a",
+    "url": "/drop_react_app_devshree/static/js/runtime~main.3077401c.js"
+  }
+]);
